b35eab9f31fea99aa095eaedeccd91da3cf8957c
Mohad009 <26S2053@utas.edu.om>
2024-03-05 09:30:00 +0400

    some slight changes

--- FlutterProject/ex2/lib/main.dart
+++ FlutterProject/ex2/lib/main.dart
@@ -22,44 +22,46 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("First Equation"),centerTitle: true,backgroundColor: Colors.amberAccent,),
+      appBar: AppBar(title: const Text("First Equation"),centerTitle: true,backgroundColor: Colors.amberAccent,),
       body:Container( 
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child:ListView(children: [
         TextFormField(
           controller: _iniV,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             labelText: "Initial Velocity",
             prefix: Icon(Icons.telegram),
             border: OutlineInputBorder(),
           ),
         ),
-        SizedBox(height: 20,),
+        const SizedBox(height: 20,),
            TextFormField(
           controller: _accel,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             labelText: "Acceleration",
             prefix: Icon(Icons.data_array),
             border: OutlineInputBorder(),
           ),
         ),
-        SizedBox(height: 20,),
+        const SizedBox(height: 20,),
              TextFormField(
           controller: _time,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             labelText: "Time",
             prefix: Icon(Icons.home),
             border: OutlineInputBorder(),
           ),
         ),
-        SizedBox(height: 20,),
-        OutlinedButton(onPressed: (){
+        const SizedBox(height: 20,),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(backgroundColor: Colors.amber,minimumSize: Size(100,50)),
+          onPressed: (){
           setState(() {
             fvel= double.parse(_iniV.text) + (double.parse(_accel.text) * double.parse(_time.text));
           });
         }, 
-        child: Text("Final Velocity",style: TextStyle(fontSize: 20,color: Colors.blue),),),
-        SizedBox(height: 20,),
+        child: const Text("Final Velocity",style: TextStyle(fontSize: 20,color: Colors.blue),),),
+        const SizedBox(height: 20,),
         Text("Final Velocity: "+fvel.toString())
       ],),)
     );
