41ea7b85c6cfc75764ae2c7c42c63eafe839481a
Mohad009 <26S2053@utas.edu.om>
2024-04-21 11:21:10 +0400

    act15 login page with firebase

---
+++ FlutterProject/act15/lib/login_page.dart
@@ -0,0 +1,87 @@
+import 'package:act15/homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _user = TextEditingController();
+  TextEditingController _pass = TextEditingController();
+
+  Future SignIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _user.text.trim(), password: _pass.text.trim());
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      padding: EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Welcome",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            controller: _user,
+            decoration: const InputDecoration(
+                labelText: "User Name",
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            controller: _pass,
+            decoration: const InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.password),
+                border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, foregroundColor: Colors.white),
+              onPressed: () {
+                SignIn();
+              },
+              child: Text("Sign In".toUpperCase())),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Not Register? ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              GestureDetector(
+                onTap: () => print('Hello'),
+                child: const Text('Register',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue)),
+              )
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
---
+++ FlutterProject/act15/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:act15/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyA0SFZyCmYpOEFHKfB_xrCjU68rpf5fwzE',
+          appId: '1:869233426454:android:1bb37fa9389edfee8a3537',
+          messagingSenderId: '869233426454',
+          projectId: 'act15-88252'));
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginPage(),
+    );
+  }
+}
