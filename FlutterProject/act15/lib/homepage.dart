dcce55c42ddaa0a9e6af2c44f989e33e3170b264
Mohad009 <26S2053@utas.edu.om>
2024-05-04 23:09:54 +0400

    act15 has all crud operation working just fine

--- FlutterProject/act15/lib/homepage.dart
+++ FlutterProject/act15/lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:act15/all_books.dart';
 import 'package:act15/insert_data.dart';
 import 'package:flutter/material.dart';
 
@@ -12,22 +13,40 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home"),centerTitle: true,backgroundColor: Colors.blue[900],),
-      body: Center(child: Column(
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+          child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-         GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>InsertData()));
-          },
-          child: const Image(image: AssetImage("assets/add.jpg"),height: 100,width: 80,),
-         ),
-         const SizedBox(height: 20,),
-         GestureDetector(
-          onTap: () {
-          },
-          child: const Image(image: AssetImage("assets/search.jpg"),height: 100,width: 80,),
-         ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => InsertData()));
+            },
+            child: const Image(
+              image: AssetImage("assets/add.jpg"),
+              height: 100,
+              width: 80,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Allbooks()));
+            },
+            child: const Image(
+              image: AssetImage("assets/search.jpg"),
+              height: 100,
+              width: 80,
+            ),
+          ),
         ],
       )),
     );
